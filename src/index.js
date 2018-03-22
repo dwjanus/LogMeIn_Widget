859c5ff4904700c4868ce400782e08a871f51013
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2018-03-22 13:11:07 -0400

    initial commit - getting it up and running

--- src/index.js
+++ src/index.js
@@ -4,18 +4,26 @@ import bodyParser from 'body-parser'
 import util from 'util'
 
 const app = express()
+const port = process.env.port || 8080
+
+if (!port) {
+  console.log('Error: Port not found')
+  process.exit(1)
+}
+
+app.set('port', port)
 
 app.use(express.static(path.join(__dirname, '/../public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ type: 'application/json' }))
 
 app.get('/', (req, res) => {
-  res.send('index.html')
-  res.status(200)
+  res.sendFile('index.html')
 })
 
-app.listen(8080, () => {
-  console.log('> App listening on port: 8080')
+const server = app.listen(app.get('port'), () => {
+  console.log(`> App listening on port: ${server.address().port}`)
+  console.log('Press Ctrl+C to quit')
 })
 
 
