21ba8f25f39288faeddbba7203e86d3ab887fd70
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2018-03-22 12:48:54 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import path from 'path'
 import bodyParser from 'body-parser'
-import cors from 'cors'
 import util from 'util'
 
 const app = express()
@@ -9,9 +8,10 @@ const app = express()
 app.use(express.static(path.join(__dirname, '/../public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ type: 'application/json' }))
-app.use(cors())
-app.options('*', cors())
 
 app.listen(8080, () => {
   console.log('> App listening on port: 8080')
 })
+
+
+
