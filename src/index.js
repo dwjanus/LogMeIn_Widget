75caca9988bb3fe3cf1ce41dcd890f481ff2689b
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2018-03-23 16:11:38 -0400

    attempting express html layout

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.set('view engine', 'html')
 app.set('layout', 'layout')
 app.enable('view cache')
 app.engine('html', require('hogan-express'))
-app.set('views', path.join(__dirname, './../views'))
+app.set('views', path.join(__dirname, './views'))
 
 app.use(express.static(path.join(__dirname, './../public')))
 app.use(bodyParser.urlencoded({ extended: true }))
