d4cb3ac1d6325f2fcd9b77773dc98176390e5f0b
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2018-04-03 15:02:22 -0400

    added teamviewer and attempting oauth

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import bodyParser from 'body-parser'
 require('body-parser-xml')(bodyParser)
 import util from 'util'
 import cors from 'cors'
+import monk from 'monk'
 
 const app = express()
 const port = process.env.port ||  process.env.PORT || 8000
+const db = monk(process.env.MONGODB_URI)
+const teamviewer = db.get('teamviewer') // used for storage of teamviewer oauth data
+
 
 if (!port) {
   console.log('Error: Port not found')
@@ -32,13 +36,15 @@ app.options('*', cors())
 app.get('/', (req, res) => {
   res.locals = {
     harvest_token: process.env.HARVEST_TOKEN,
-    harvest_account: process.env.HARVEST_ACCOUNT
+    harvest_account: process.env.HARVEST_ACCOUNT,
+    tv_id: process.env.TEAMVIEWER_ID
   }
   res.render('layout', {
     partials: { 
       logmein: 'logmein.html',
       bomgar: 'bomgar.html',
-      harvest: 'harvest.html'
+      harvest: 'harvest.html',
+      teamviewer: 'teamviewer.html'
    }
   })
 })
@@ -47,6 +53,12 @@ app.get('/', (req, res) => {
 app.post('/data', (req, res) => {
   console.log(`[POST] at /data ==> request: ${util.inspect(req.body)}`)
   res.send(200)
+})
+
+app.post('/tv/oauth/', (req, res) => {
+  console.log('>>> saving oauth token')
+  console.log(`>>> data: \n${util.inspect(req.body)}`)
+  // teamviewer.insert()
 })
 
 
