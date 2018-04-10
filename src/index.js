53f48c951efcddbcbd47ed1d18024755f6d6d5c8
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2018-04-10 11:27:58 -0400

    got oauth to work - now reconfigured with different permission set and added possible handler

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.engine('html', require('hogan-express'))
 app.set('views', path.join(__dirname, './../views'))
 
 app.use(express.static(path.join(__dirname, './../public')))
-app.use(express.static(path.join(__dirname, './../public/img')))
+app.use(express.static(path.join(__dirname, './../public/images')))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ type: 'application/json' }))
 app.use(bodyParser.xml({xmlParseOptions: {
