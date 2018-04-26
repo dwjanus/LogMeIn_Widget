75a7693c7750b88768a8df92878778b9a5f30e78
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2018-04-26 13:19:11 -0400

    bootstrapped for the most part, logmein and harvest work as written

--- src/index.js
+++ src/index.js
@@ -61,7 +61,6 @@ app.get('/', (req, res) => {
     partials: {
       ref_console: 'ref_console.html',
       logmein: 'logmein_new.html',
-      bomgar: 'bomgar.html',
       harvest_time: 'harvest_time.html',
       teamviewer: 'teamviewer.html'
     }
